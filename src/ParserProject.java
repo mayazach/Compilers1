d59d1481fba9f9df6f0791981da653b110272276
maya <mayazaha2@gmail.com>
2017-11-03 17:08:48 +0200

    Finished everything except error messages

--- src/ParserProject.java
+++ src/ParserProject.java
@@ -1,6 +1,156 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.text.ParseException;
 
 public class ParserProject {
 	public static void main(String[] args){
-		System.out.println("Hello parser!");
+	    DataInputStream in = new DataInputStream(System.in);
+		Parser parser = new Parser(in);
+        try {
+            System.out.println(parser.Parse());
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
 	}
+}
+
+class Parser{
+    char lookahead;
+    DataInputStream a;
+
+    Parser(DataInputStream a){
+        this.a = a;
+        try {
+            lookahead = (char) a.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    int Parse() throws ParseException{
+        try {
+            return Exp();
+        } catch (ParseException e) {
+            throw new ParseException("Parsing failed",1);
+        }
+    }
+
+    void consume(int symbol) throws ParseException,IOException{
+        if(lookahead != symbol) {
+            System.out.println(lookahead);
+            throw new ParseException("Unexpected symbol found", 1);
+        }
+        lookahead = (char) a.read();
+    }
+
+    int Exp() throws ParseException{
+        int term;
+        try {
+            term = Term();
+            return Rest(term);
+        } catch (ParseException e) {
+            throw new ParseException("Exp failed",1);
+        }
+    }
+
+    int Rest(int term) throws ParseException{
+        if(lookahead == '+'){
+            try {
+                this.consume(lookahead);
+            } catch (ParseException | IOException e) {
+                throw new ParseException("Rest failed",1);
+            }
+            try {
+                term += Term();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return Rest(term);
+        }
+        else if(lookahead == '-'){
+            try {
+                this.consume(lookahead);
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                term -= Term();
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            return Rest(term);
+        }
+        else
+            return term;
+    }
+
+    int Term() throws ParseException{
+        int factor;
+        try {
+            factor = Factor();
+            return Term2(factor);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            throw new ParseException("Term failed",1);
+        }
+    }
+
+    int Term2(int factor) throws ParseException{
+        if(lookahead == '*'){
+            try {
+                this.consume(lookahead);
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                factor *= Factor();
+            } catch (ParseException e) {
+                throw new ParseException("Term2 failed",1);
+            }
+            return Term2(factor);
+        }
+        else if(lookahead == '/'){
+            try {
+                this.consume(lookahead);
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                factor /= Factor();
+            } catch (ParseException e) {
+                throw new ParseException("Term2 failed",1);
+            }
+            return Term2(factor);
+        }
+        else
+            return factor;
+    }
+
+    int Factor() throws ParseException{
+        if(lookahead == '('){
+            try {
+                this.consume(lookahead);
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+            int result = Exp();
+            try {
+                this.consume(')');
+            } catch (ParseException | IOException e) {
+                e.printStackTrace();
+            }
+            return result;
+        }
+        else if(Character.isDigit(lookahead)){
+                int value = Character.getNumericValue(lookahead);
+                try {
+                    this.consume(lookahead);
+                    return value;
+                } catch (ParseException | IOException e) {
+                    throw new ParseException("Could not read symbol",1);
+                }
+        }
+        else
+            throw new ParseException("Unexpected terminal",1);
+        }
 }
